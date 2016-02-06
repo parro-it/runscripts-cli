103432bc394c624f6f96215089e184d5648ef962
Andrea Parodi <andrea@parro.it>
2016-02-06 21:55:22 +0100

    usage with command list

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 'use strict';
 
 const yargs = require('yargs');
@@ -6,15 +8,41 @@ const chalk = require('chalk');
 
 const argv = yargs.argv;
 const command = argv._[0];
-process.stdout.write(`\nExecuting command ${chalk.bold.yellow(command)}.\n`);
 
-runscripts(command, argv)
-  .then(() => process.stdout.write(`\nDone ${chalk.bold.yellow(command)}.\n`))
-  .catch(err => {
-    if (err.code === 'ENOSCRIPT' || err.code === 'ENOCONFIG') {
-      process.stderr.write(`\n${chalk.bold.red(err.code)}: ${err.message}\n`);
-      process.exit(-1);
-    }
 
-    process.stderr.write(`\n${err.stack}\n`);
-  });
+const handleError = err => {
+  if (err.code === 'ENOSCRIPT' || err.code === 'ENOCONFIG') {
+    process.stderr.write(`\n${chalk.bold.red(err.code)}: ${err.message}\n`);
+    process.exit(-1);
+  }
+
+  process.stderr.write(`\n${err.stack}\n`);
+};
+
+if (!command) {
+  runscripts.readScriptsObject()
+    .then(scripts => {
+      process.stdout.write(`
+Usage: runs <command-name> [...command-options]
+
+Available commands:
+${
+  '\n * ' +
+  Object.keys(scripts.object)
+  .join('\n * ')
+}
+`);
+      process.exit(0);
+    })
+    .catch(handleError);
+
+} else {
+
+  process.stdout.write(`\nExecuting command ${chalk.bold.yellow(command)}.\n`);
+
+
+  runscripts(command, argv)
+    .then(() => process.stdout.write(`\nDone ${chalk.bold.yellow(command)}.\n`))
+    .catch(handleError);
+
+}
